b6feff202c26088b116992777ba868c963bf9d89
datenpunk <daniel@khan.io>
2015-09-01 16:42:29 +0200

    Added GC profiling

---
+++ monitoring/gc.js
@@ -0,0 +1,33 @@
+var gcprofiler = require('gc-profiler');
+var fs = require('fs');
+
+
+
+module.exports.init = function (time) {
+
+
+    fs.writeFile("/tmp/gc_Scavenge", 'Start;Duration\n', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+
+    fs.writeFile("/tmp/gc_MarkSweepCompact", 'Start;Duration\n', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+
+    // Listen to GC events
+    gcprofiler.on('gc', function (info) {
+
+        var diff = process.hrtime(time);
+        var ms = diff[0] * 1e9 + diff[1];
+
+        fs.appendFile("/tmp/gc_", (ms - info.duration * 1000) + ';' + (info.duration * 1000) + "\n", function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+};
